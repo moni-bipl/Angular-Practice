31a0d11c8124cbcc648ccebdfee372ae509508dc
Moni-Chaurasiya <monichaurasiya993@gmail.com>
2025-12-28 23:00:08 +0530

    learned some basic concepts

--- angular-basic/src/app/app.ts
+++ angular-basic/src/app/app.ts
@@ -5,28 +5,57 @@ import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [Login,Profile],
+  imports: [Login, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('angular-basic');//signal() is a reactive state container.  [A variable that Angular automatically tracks and updates the UI whenever its value changes.]
-  name="Moni Chaurasiya"  // these are properties of class 
-  hello(){
-    let a= 1;  // we can define variable inside function but not directly inside class 
+  name = "Moni Chaurasiya"  // these are properties of class 
+  hello() {
+    let a = 1;  // we can define variable inside function but not directly inside class 
   }
-  a=1
-  b=2
-  c="2"
- names:String="Moni"
- data:string|number=8
-  handleClickEvent(){
-     alert("Function called");
-     this.otherFunction() // this refer to this class 
-     console.log(this.name);
-     console.log(this.data);
+  a = 1
+  b = 2
+  c = "2"
+  names: String = "Moni"
+  data: string | number = 8
+  handleClickEvent() {
+    alert("Function called");
+    this.otherFunction() // this refer to this class 
+    console.log(this.name);
+    console.log(this.data);
   }
-  otherFunction(){
+  otherFunction() {
     console.log("otherFunction");
+  }
+
+
+  count = 0
+  handleIncrement() {
+    this.count = this.count + 1
+  }
+  handleDecrement() {
+    this.count--
+  }
+  handleReset() {
+    this.count = 0
+  }
+
+  handleCounter(val: string) {
+    if (val == 'plus') {
+      this.count++
+    } else if (val == 'minus') {
+      this.count--
+    } else {
+      this.count = 0
+    }
+  }
+  
+  handleEvent(event:any){
+    console.log("function called", event.type);
+    console.log("function called", (event.target as HTMLElement).classList);
+    console.log("function called", (event.target as HTMLElement).className);
+
   }
 }
--- angular-basic/src/app/profile/profile.ts
+++ angular-basic/src/app/profile/profile.ts
@@ -3,9 +3,22 @@ import { Component } from "@angular/core";
     selector:'app-profile',
     // template:`<h1>Profile Component</h1>`
       templateUrl: './profile.html',
-      styleUrl: './profile.css'
+     // styleUrl: './profile.css'
+      styleUrls:['./profile.css','./profilebtn.css']
+     // styles:['h1{background-color: white}']
 
 })
 export class Profile{
-
+  display=true
+  x=10
+  hide(){
+    this.display=!this.display
+  }
+  color=1
+  handleChange(val:number){
+     this.color=val;
+  }
+  handleInput(event:Event){
+      this.color=parseInt((event.target as HTMLInputElement).value)
+  }
 }
